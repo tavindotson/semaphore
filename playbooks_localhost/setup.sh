555bf423d7d66851c92cf8f2140da76b3675011c
Tavin Dotson <tjd@tavin.xyz>
2024-09-17 20:22:48 -0700

    added generic setup file

---
+++ playbooks_localhost/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Create a user first, then run this as the user you want to setup."
+
+read -p "Are you sure you want to continue? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Continuing..."
+else
+    echo "Exiting..."
+    exit 1
+fi
+
+ansible-playbook setup.yml --ask-become-pass
