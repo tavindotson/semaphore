a7eb14b0b30942ac1dbc22b328a7405b22e0f455
Tavin Dotson <tjd@tavin.xyz>
2024-09-21 22:56:23 -0700

    added a note on steps user needs to take manually

--- playbooks_localhost/setup.sh
+++ playbooks_localhost/setup.sh
@@ -16,3 +16,12 @@ fi
 sudo apt update
 sudo apt install ansible -y
 ansible-playbook setup.yml --ask-become-pass
+echo
+echo "Done"
+echo
+echo "Do the following manually:"
+echo "  - Start UFW"
+echo "  - Edit .zshrc to include plugins"
+echo "  - Add .hushlogin file"
+echo "  - Remove /etc/motd"
+echo
